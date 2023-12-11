45591a6b49759028ee5b55ff772f6ae0ad9d1933
Castro32 <okanga.fidel@gmail.com>
2023-12-11 21:58:56 +0300

    Updated Header component styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
 
     return (
         <section className="h-wrapper">
-            <div className="flexCenter paddings innerWidth h-container">
+            <div className="flexEnd paddings innerWidth h-container">
                 <img src="./CAS HOMES.jpg" alt="logo" width={100} />
-                <div className="innerWidth flexCenter">
+                <div className="innerWidth flexEnd">
     <div className="menu-icon" onClick={handleMenuToggle}>
         <BiMenuAltRight size={30} />
     </div>
@@ -25,10 +25,11 @@ const Header = () => {
     >
 <div className="h-menu" style={{ right: menuOpened ? '0' : '-100%' }}>
         <a href="Hero" style={{gap:'10px'}}>Home</a>
-        <a href="#Residencies"  style={{padding:'100px'}}>Residencies</a>
-        <a href="Values" style={{gap:'10px', paddingRight:'100px'}}>Our Values</a>
+        <a href="#Residencies"  style={{padding:'1px'}}>Residencies</a>
+        <a href="#Contact"  style={{padding:'1px'}}>Contact Us</a>
+        <a href="Values" style={{gap:'10px', paddingRight:'10px'}}>Our Values</a>
         <button className="button" >
-            <a href="Contact" style={{gap:'100px'}}>Contact Us</a>
+            <a href="Sign In" style={{gap:'100px'}}>Sign In</a>
         </button>
     </div>
     </OutsideClickHandler>
