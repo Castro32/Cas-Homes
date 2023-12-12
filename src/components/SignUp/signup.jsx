1cf5284c53ba790487b85a0456d9ff6811db01eb
IsoDevMate <oumaoduor5827@gmail.com>
2023-12-12 10:38:45 +0300

    added routing for signup page

--- src/components/SignUp/signup.jsx
+++ src/components/SignUp/signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './signup.css'
 
-import user_icon from '..'
+
 const SignUp =()=>{
 
     const [action,setAction] = useState("SIgn Up");
@@ -13,16 +13,16 @@ const SignUp =()=>{
             </div>
             <div className="inputs">
                 {action==="Log-In"? <div></div>:<div className="input">
-                    <img src="public/profile_3135715.png" alt=""/>
+                   
                     <input type='text' placeholder='Name'/>
                 </div>}
                 
                 <div className="input">
-                    <img src='public/mail_646135.png' alt=''/>
+                    
                     <input type='email' placeholder='Email Id'/>
                 </div>
                 <div className="input">
-                    <img src='public/password_159478.png' alt=''/>
+                   
                     <input type='password' placeholder='Password'/>
                 </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { App } from "./App";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+  <App />
+</React.StrictMode>
+
 );
