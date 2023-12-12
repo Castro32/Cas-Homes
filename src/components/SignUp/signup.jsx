41d9188df45b5962a21f1550b12dcb41d41f9a2e
Castro32 <okanga.fidel@gmail.com>
2023-12-12 15:22:06 +0300

    added toggle password functionality

--- src/components/SignUp/signup.jsx
+++ src/components/SignUp/signup.jsx
@@ -1,38 +1,69 @@
-import React, { useState } from 'react'
-import './signup.css'
+import React, { useState } from 'react';
+import './signup.css';
 
+const SignUp = () => {
+  const [action, setAction] = useState('Sign Up');
+  const [showPassword, setShowPassword] = useState(false);
 
-const SignUp =()=>{
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-    const [action,setAction] = useState("SIgn Up");
-    return(
-        <div className='container'>
-            <div className="header">
-                <div className="text">{action}</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                {action==="Log-In"? <div></div>:<div className="input">
-                   
-                    <input type='text' placeholder='Name'/>
-                </div>}
-                
-                <div className="input">
-                    
-                    <input type='email' placeholder='Email Id'/>
-                </div>
-                <div className="input">
-                   
-                    <input type='password' placeholder='Password'/>
-                </div>
-            </div>
-            {action ==="Sign-Up"?<div></div>:<div className="forgot-password">Forgot Password?<span>Click Here!</span></div>}
-            
-            <div className="submit-container">
-                <div className={action==="Log-In"?"submit gray":"submit"} onClick={()=>{setAction("Sign-Up")}}>Sign-Up</div>
-                <div className={action==="Sign-Up"?"submit gray":"submit"} onClick={()=>{setAction("Log-In")}}>Log-In</div>
-            </div>
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>{action}</div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+        {action === 'Log-In' ? <div></div> : <div className='input'>
+          <img src='./profile_3135715.png' alt='Profile' />
+          <input type='text' placeholder='Enter full name' />
+        </div>}
+
+        <div className='input'>
+          <img src='./mail_646135.png' alt='Email' />
+          <input type='email' placeholder='example@gmail.com' />
         </div>
-    )
-}
-export default SignUp
+
+        <div className='input'>
+          <img src='./password_159478.png' alt='Password' />
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Password'
+          />
+          <div className='password-toggle-icon' onClick={togglePasswordVisibility}>
+            {showPassword ? (
+              <img src='./eyes_5904983.png' alt='Hide Password' />
+            ) : (
+              <img src='./view_402758.png' alt='Show Password' />
+            )}
+          </div>
+        </div>
+      </div>
+
+      {action === 'Sign-Up' ? <div></div> :
+        <div className='forgot-password'>
+          Forgot Password?<span>Click Here!</span>
+        </div>
+      }
+
+      <div className='submit-container'>
+        <div
+          className={action === 'Log-In' ? 'submit gray' : 'submit'}
+          onClick={() => { setAction('Sign-Up') }}
+        >
+          Sign-Up
+        </div>
+        <div
+          className={action === 'Sign-Up' ? 'submit gray' : 'submit'}
+          onClick={() => { setAction('Log-In') }}
+        >
+          Log-In
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
