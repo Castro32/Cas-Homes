62c909410ee40b3709d4fdaa76779c14c42ce703
Castro32 <okanga.fidel@gmail.com>
2024-01-14 22:02:41 +0300

    updated the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 
-
-
-
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-  <App />
-</React.StrictMode>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
